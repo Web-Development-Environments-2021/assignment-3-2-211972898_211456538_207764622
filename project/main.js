0713c397bcc9bf1496166391f6fb066d1a7e2483
Guy Arieli <guyarieli17@gmail.com>
2021-05-26 17:52:46 +0300

    another commit

--- project/main.js
+++ project/main.js
@@ -1,92 +1,60 @@
-/* get defualt port from operation system*/
-const port = process.env.PORT || 3000;
-/* region of global import */
-var express = require("express");
-
-
-/* create refrence of express*/
-var app = express();
-
-
-/* create server and listen int Port*/
-const server = app.listen(port, () => {
-  console.log(`Server listen on port ${port}`);
-});
-
-/* all talk with client by path */
-app.get('/', (req, res) => {
-  console.log('Someone logied in');
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //#region global imports
 // const DButils = require("./routes/utils/DButils");
-// const axios = require("axios");
-// const bcrypt = require("bcryptjs");
-// require("dotenv").config();
-// //#endregion
-// //#region express configures
-// var path = require("path");
-// const session = require("client-sessions");
-// var logger = require("morgan");
-// var cors = require("cors");
+const axios = require("axios");
+const bcrypt = require("bcryptjs");
+require("dotenv").config();
+//#endregion
+//#region express configures
+var express = require("express");
+var path = require("path");
+const session = require("client-sessions");
+var logger = require("morgan");
+var cors = require("cors");
 
-
-// app.use(logger("dev")); //logger
-// app.use(express.json()); // parse application/json
-// app.use(
-//   session({
-//     cookieName: "session", // the cookie key name
-//     secret: process.env.COOKIE_SECRET, // the encryption key
-//     duration: 24 * 60 * 60 * 1000, // expired after 20 sec
-//     activeDuration: 1000 * 60 * 5, // if expiresIn < activeDuration,
-//     cookie: {
-//       httpOnly: false,
-//     },
-//     //the session will be extended by activeDuration milliseconds
-//   })
-// );
-// app.use(express.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
-// app.use(express.static(path.join(__dirname, "public"))); //To serve static files such as images, CSS files, and JavaScript files
+var app = express();
+app.use(logger("dev")); //logger
+app.use(express.json()); // parse application/json
+app.use(
+  session({
+    cookieName: "session", // the cookie key name
+    secret: process.env.COOKIE_SECRET, // the encryption key
+    duration: 24 * 60 * 60 * 1000, // expired after 20 sec
+    activeDuration: 1000 * 60 * 5, // if expiresIn < activeDuration,
+    cookie: {
+      httpOnly: false,
+    },
+    //the session will be extended by activeDuration milliseconds
+  })
+);
+app.use(express.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
+app.use(express.static(path.join(__dirname, "public"))); //To serve static files such as images, CSS files, and JavaScript files
 
 // middleware to serve all the needed static files under the dist directory - loaded from the index.html file
 // https://expressjs.com/en/starter/static-files.html
-// app.use(express.static("dist"));
+app.use(express.static("dist"));
 
-// app.get("/api", (req, res) => {
-//   res.sendFile(__dirname + "/index.html");
-// });
+app.get("/api", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
-// const corsConfig = {
-//   origin: true,
-//   credentials: true,
-// };
+const corsConfig = {
+  origin: true,
+  credentials: true,
+};
 
-// app.use(cors(corsConfig));
-// app.options("*", cors(corsConfig));
+app.use(cors(corsConfig));
+app.options("*", cors(corsConfig));
 
+const port = process.env.PORT || "3000";
 
 // const auth = require("./routes/auth");
 // const users = require("./routes/users");
 // const league = require("./routes/league");
 // const teams = require("./routes/teams");
 
-//#endregion
+// //#endregion
 
-//#region cookie middleware
+// //#region cookie middleware
 // app.use(function (req, res, next) {
 //   if (req.session && req.session.user_id) {
 //     DButils.execQuery("SELECT user_id FROM users")
@@ -101,12 +69,12 @@ app.get('/', (req, res) => {
 //     next();
 //   }
 // });
-//#endregion
+// //#endregion
 
-// ----> For cheking that our server is alive
+// // ----> For cheking that our server is alive
 // app.get("/alive", (req, res) => res.send("I'm alive"));
 
-// Routings
+// // Routings
 // app.use("/users", users);
 // app.use("/league", league);
 // app.use("/teams", teams);
@@ -117,10 +85,12 @@ app.get('/', (req, res) => {
 //   res.status(err.status || 500).send(err.message);
 // });
 
-
-
-// process.on("SIGINT", function () {
-//   if (server) {
-//     server.close(() => console.log("server closed"));
-//   }
+// const server = app.listen(port, () => {
+//   console.log(`Server listen on port ${port}`);
 // });
+
+// // process.on("SIGINT", function () {
+// //   if (server) {
+// //     server.close(() => console.log("server closed"));
+// //   }
+// // });
