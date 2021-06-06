173d242f08d9b708c4d309fdce2221bc51ec8046
Noam Refeali <73943903+cecroud@users.noreply.github.com>
2021-06-06 22:45:37 +0300

    fix no player
    number detection fix

--- project/routes/player.js
+++ project/routes/player.js
@@ -9,7 +9,8 @@ const players_utils = require("./utils/players_utils");
 router.get("/playerFullDetails/:playerId", async (req, res, next) => {
   try {
     const player_id = req.params.playerId;
-    if(typeof(player_id) != 'number'){throw new Error("Player Id should be a number");}
+    let isnum = /^\d+$/.test(player_id);
+    if(!isnum){throw {status:409, message:'player_Id should be a number'};}
     const players_details = await players_utils.getAllPlayerInfoById(player_id);
     res.send(players_details);
   } catch (error) {next(error);}
